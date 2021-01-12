d74fd0ecb2dcf631222bd3bc858bb9e8948d3925
SungminSo <soos196@hanyang.ac.kr>
2021-01-12 22:56:04 +0900

    flatten 추가

---
+++ tensorflow_102/flatten/reshape.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import pandas as pd
+
+# 데이터 준비
+(independent_var, dependent_var), _ = tf.keras.datasets.mnist.load_data()
+
+# 60000x28x28 -> 60000x784
+independent_var = independent_var.reshape(60000, 784)
+
+# one-hot encoding
+dependent_var = pd.get_dummies(dependent_var)
+
+print(independent_var.shape, dependent_var.shape)
+
+# 모델 구조 생성
+X = tf.keras.layers.Input(shape=[784])
+H = tf.keras.layers.Dense(84, activation='swish')(X)
+Y = tf.keras.layers.Dense(10, activation='softmax')(H)
+
+model = tf.keras.models.Model(X, Y)
+model.compile(loss='categorical_crossentropy', metrics='accuracy')
+
+# 모델 학습
+model.fit(independent_var, dependent_var, epochs=10)
+
+# 모델 이용
+print("Predicions: ", model.predict(independent_var[:5]))
